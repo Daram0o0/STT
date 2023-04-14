02b7df35fa7d9b48eb6ee4a5aa431c47e02bc4b6
whisper74541 <zaq4869@naver.com>
2023-04-14 23:02:42 +0900

    test

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -4,19 +4,16 @@ import './styles.css';
 function Home() {
     return(
         <div className='Home'>
-            <div className='top'>
-                홈화면
-            </div>
-            <div className='middle'>
+            <div className='top'> 홈화면 </div>
+            <div className='middle'> 
                 여기에 공용 시간표가 보이게 하기
+                <div>
+                    
+                </div>
             </div>
             <div className='bottom-box'>
-                <button type='button' className='bottom-my' onClick={() => {console.log("내 시간표")}}>
-                    내 시간표
-                </button>
-                <button type='button' className='bottom-home' onClick={() => {console.log("Home")}}>
-                    Home
-                </button>
+                <button type='button' onClick={() => {console.log("내 시간표")}}> 내 시간표 </button>
+                <button type='button' onClick={() => {console.log("Home")}}> Home </button>
                 <button type='button' onClick={() => {console.log("설정")}}> 설정 </button>
             </div>
         </div>
