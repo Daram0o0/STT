b23e5ab236f7024248a5d0bcd1f2bb6af3f4893a
CaKlan <lhs74187@gmail.com>
2023-04-16 16:44:27 +0900

    signup 기능 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,15 @@ import './App.css';
 import { BrowserRouter , Routes, Route} from 'react-router-dom';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ front/src/pages/Signup/index.tsx
@@ -0,0 +1,52 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import './styles.css';
+import { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../components/firebase';
+
+
+function Signup(){
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = async (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                //Signed in
+                const user = userCredential.user;
+                console.log(user);
+                navigate("/login");
+            })
+            .catch((err) => {
+                const errorCode = err.code;
+                const errorMsg = err.message;
+                console.log(errorCode, errorMsg);
+            })
+    }
+
+    return (
+        <div className="Signup">
+            <div className="body">
+                <div className="deco_title"></div>
+
+                <h1 className="title">회원 가입</h1>
+
+                <div className="signup_form">
+                    <input className="id_input" value={email} onChange={(e)=>{setEmail(e.target.value)}} type="text" placeholder='email'></input>
+                    <input className="pw_input" value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='password'></input>
+                    <br/><br/>
+
+                    <button className="submit" onClick={(e)=>{onSubmit(e)}}>가입하기</button>                   
+                </div>
+                
+                <div className="deco_end"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
