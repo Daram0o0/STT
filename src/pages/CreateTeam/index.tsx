be1e61e9c5fd8d23afb8e626fe93ad902b6b02fa
Daram0o0 <c30728535@gmail.com>
2023-05-23 01:58:33 +0900

    invitePage

--- src/pages/CreateTeam/index.tsx
+++ src/pages/CreateTeam/index.tsx
@@ -4,10 +4,30 @@ import TimeCell from "../../components/TimeCell/index";
 
 function CreateTeam() {
     return (
-        <div>
-            hello
-            <select></select>
-            <TimeCell/>
+        <div className="CreateTeam">
+            <div className="container">
+
+                <div className="team_name">
+                    <div>팀 이름</div>
+                    <input type="text" placeholder="팀 명"></input>
+                </div>
+                    
+                <div className="team_explain">
+                    <div>팀 설명</div>
+                    <textarea placeholder="simple is best!"></textarea>
+                </div>
+
+                <div className="invite">
+                    <div>초대 링크</div>
+                    <p>http://localhost:3000/STT/createteam</p>
+                    <button>Invite Code</button>
+                </div>
+
+                <div className="make">
+                    <button>make</button>
+                </div>
+                
+            </div>
         </div>
     )
 }
