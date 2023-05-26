20b4a3b18d3ce692a6725486400e86a0a86a6522
whisper74541 <zaq4869@naver.com>
2023-05-26 22:52:17 +0900

    시간표 모달 중간

--- src/pages/Modal/OpenModal.tsx
+++ src/pages/Modal/OpenModal.tsx
@@ -30,28 +30,61 @@ function ModalDetail() {
         //     <div>추가하기</div>
         // </form>
         <form className="formcon">
-            <dl className="dltest">
-                <dt>과목명</dt>
-                <dd>
+            <div className="container">
+                <div className="subject">과목명
                     <input type={"text"} placeholder="강의명"></input>
-                </dd>
-                <dt>시간 / 장소</dt>
-                <dd className="ddtest">
-                    <div className="divtest">
-                        <ol className="oltest">
-                            <li className={ test == 1 ? "active" : ""} onClick={() => {setTest(1)}}>월</li>
-                            <li className={ test == 2 ? "active" : ""} onClick={() => {setTest(2)}}>화</li>
-                            <li className={ test == 3 ? "active" : ""} onClick={() => {setTest(3)}}>수</li>
-                            <li className={ test == 4 ? "active" : ""} onClick={() => {setTest(4)}}>목</li>
-                            <li className={ test == 5 ? "active" : ""} onClick={() => {setTest(5)}}>금</li>
-                            <li className={ test == 6 ? "active" : ""} onClick={() => {setTest(6)}}>토</li>
-                            <li className={ test == 7 ? "active" : ""} onClick={() => {setTest(7)}}>일</li>
-                        </ol>
-                        <input type={"text"} placeholder="장소"></input>
-
-                    </div>
-                </dd>
-            </dl>
+                </div>
+                <div className="time/place">시간 / 장소
+                    <input type={"text"} placeholder="장소"></input>
+                </div>
+                <div className="select">
+                    <ol className="ol">
+                        <li className={ test == 1 ? "active" : ""} onClick={() => {setTest(1)}}>월</li>
+                        <li className={ test == 2 ? "active" : ""} onClick={() => {setTest(2)}}>화</li>
+                        <li className={ test == 3 ? "active" : ""} onClick={() => {setTest(3)}}>수</li>
+                        <li className={ test == 4 ? "active" : ""} onClick={() => {setTest(4)}}>목</li>
+                        <li className={ test == 5 ? "active" : ""} onClick={() => {setTest(5)}}>금</li>
+                        <li className={ test == 6 ? "active" : ""} onClick={() => {setTest(6)}}>토</li>
+                        <li className={ test == 7 ? "active" : ""} onClick={() => {setTest(7)}}>일</li>
+                        <div className="timeselect">
+                            {/* <label></label> */}
+                            <select className="starttime">
+                                <option value={"9"}>9시</option>
+                                <option value={"10"}>10시</option>
+                                <option value={"11"}>11시</option>
+                                <option value={"12"}>12시</option>
+                                <option value={"13"}>13시</option>
+                                <option value={"14"}>14시</option>
+                                <option value={"15"}>15시</option>
+                                <option value={"16"}>16시</option>
+                                <option value={"17"}>17시</option>
+                                <option value={"18"}>18시</option>
+                                <option value={"19"}>19시</option>
+                                <option value={"20"}>20시</option>
+                                <option value={"21"}>21시</option>
+                            </select>
+                            ~
+                            <select className="endtime">
+                                <option value={"9"}>9시</option>
+                                <option value={"10"} selected>10시</option>
+                                <option value={"11"}>11시</option>
+                                <option value={"12"}>12시</option>
+                                <option value={"13"}>13시</option>
+                                <option value={"14"}>14시</option>
+                                <option value={"15"}>15시</option>
+                                <option value={"16"}>16시</option>
+                                <option value={"17"}>17시</option>
+                                <option value={"18"}>18시</option>
+                                <option value={"19"}>19시</option>
+                                <option value={"20"}>20시</option>
+                                <option value={"21"}>21시</option>
+                            </select>
+                            </div>
+                    </ol>
+                    <button type="submit">저장</button>
+                </div>
+                <button type="submit">저장</button>
+            </div>
         </form>
     )
 }
