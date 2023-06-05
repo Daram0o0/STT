226ff20109b38ac14cff256cfd243ac06784f473
Daram0o0 <c30728535@gmail.com>
2023-06-05 22:04:21 +0900

    nothing changed...

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,20 +1,23 @@
 import "./styles.css";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import { createContext, useContext, useState } from 'react';
+const themeDefault = {backgroundcolor: 'black', color: 'white'};
+const themeContext = createContext(themeDefault);
 
 function Setting() {
 // 글꼴, 글자 크기, 로그아웃, 처음 시작할 페이지, 다크모드설정
+  const [isChecked, setIsChecked] = useState(false);
+  const theme = useContext(themeContext);
+  console.log(theme)
   return (
-    
-    <div className="Setting">
+    // <themeContext.Provider value={theme}>
+    <div className="Setting" style={theme}>
       <Header />
       <div className="setting_container">
       <Sidebar/>
       
       <div className="setting_subcontainer">
-        <div className="logout">
-          <div>(￣▽￣)ノ</div>
-        </div>
         <div className="setting_page">
           <div className="font">
               <div className="divname">font</div>
@@ -40,7 +43,8 @@ function Setting() {
           <div className="darkmode_btn">
             <div className="divname">dark mode</div>
             <div className="divclass">
-              <input type="checkbox" id="toggle" hidden />
+              <input type="checkbox" id="toggle" checked={isChecked} onChange={(e)=>{setIsChecked(e.target.checked)} } hidden/>
+                
               <label htmlFor="toggle" className="toggleSwitch">
                 <span className="toggleButton"></span>
               </label>
@@ -51,7 +55,7 @@ function Setting() {
         </div>
         </div>
       </div>
-      
+      // </themeContext.Provider>
   )
 }
 export default Setting
